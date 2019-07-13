94ae5bf24dfbbd14eab832c16bcf7307276e97f5
freyamade <frybrdrck@gmail.com>
2019-07-13 01:45:38 +0100

    Fixing minimization in production builds

--- webpack.config.js
+++ webpack.config.js
@@ -42,17 +42,12 @@ module.exports = {
 
 if (process.env.NODE_ENV === 'production') {
   module.exports.devtool = '#source-map'
+  module.exports.optimization = {minimize: true}
   // http://vue-loader.vuejs.org/en/workflow/production.html
   module.exports.plugins = (module.exports.plugins || []).concat([
     new webpack.DefinePlugin({
       'process.env': {
         NODE_ENV: '"production"'
-      }
-    }),
-    new webpack.optimize.UglifyJsPlugin({
-      sourceMap: true,
-      compress: {
-        warnings: false
       }
     }),
     new webpack.LoaderOptionsPlugin({
