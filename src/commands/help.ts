abb2d6f38d79e35c9fc45de656e183eeb203d83f
freyamade <frybrdrck@gmail.com>
2019-07-14 12:39:10 +0100

    Add help command and messages to all existing commands

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -18,6 +18,10 @@ function leftJustify(str: string, width: number): string {
 export class Summary extends Command {
   readonly name: string = '?'
   readonly summary: string = 'Print out this list of available commands.'
+  readonly help: string = `\x1b[32m? - Print out a list of available commands.\x1b[0m
+
+\rUsage:
+\r\t\x1b[33m?\x1b[0m`
 
   execute(term: Fresh, args: string[]) {
     // Loop through the list of commands, and print them out
@@ -34,8 +38,47 @@ export class Summary extends Command {
 export class Help extends Command {
   readonly name: string = 'help'
   readonly summary: string = 'Get help and usage instructions for the commands in this website.'
+  readonly help: string = `\x1b[32mhelp - Get instructions for commands.\x1b[0m
+
+\rUsage:
+\r\t\x1b[33mhelp [command_name]\x1b[0m
+
+\r\x1b[33m'command_name'\x1b[0m can be the name of a command in the system.
+\r\x1b[33m'command_name'\x1b[0m may also be omitted, in which case an introductory message will be printed instead.`
+  private intro: string = `\x1b[35mfreyama.de\x1b[0m - The homepage of Freya Madeline Broderick.
+
+\rBuilt to resemble a terminal, it is fully interactive and contains all of the usual things a personal website would contain, plus maybe some easter eggs!
+\rFor a list of available commands, run \x1b[33m'?'\x1b[0m`
 
   execute(term: Fresh, args: string[]) {
-    term.logError('help: Not yet implemented.')
+    // Ensure only 0 or 1 arguments are passed
+    if (args.length >= 2) {
+      term.logError(`help: Too many arguments (${args.length}). Expected 0 or 1.`)
+    }
+
+    // If 0, write out a help message for the system
+    if (args.length === 0) {
+      term.newline()
+      term.write(this.intro)
+      term.newline()
+    }
+    // If 1, find the command whose name is the same as the passed argument, and print out its help message
+    else {
+      const cmdName = args[0]
+      // Output the correct message
+      let found = false
+      Commands.forEach(cmd => {
+        if(cmd.name === cmdName) {
+          found = true
+          term.newline()
+          term.write(cmd.help)
+          term.newline()
+          return
+        }
+      })
+      if (!found) {
+        term.logError(`help: '${cmdName}' is not a valid command`)
+      }
+    }
   }
 }
