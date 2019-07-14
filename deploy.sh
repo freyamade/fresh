5098dd0383ad29b809b0fcc751f54f6e4e812910
freyamade <frybrdrck@gmail.com>
2019-07-14 19:36:03 +0100

    deploy script should be working now

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,11 @@ set -e
 # Set the NODE_ENV value for this script
 NODE_ENV=production
 
+# Checkout gh-pages and pull master into it
+git checkout gh-pages
+git fetch origin
+git rebase master
+
 # Before running the build, take the latest commit and insert it into the code
 COMMIT=$(git rev-parse --short HEAD)
 sed -i "s/{VERSION}/$COMMIT/" src/fresh.ts
@@ -13,15 +18,9 @@ sed -i "s/{VERSION}/$COMMIT/" src/fresh.ts
 # Build the static files on this branch first
 npm run build
 
-# Before checking out the gh-pages branch, hard reset the fresh.ts file
-git checkout -- src/fresh.ts
-
-# Checkout the gh-pages branch
-git checkout gh-pages
-
 # Add the necessary files for the gh-pages branch and commit them
 git add static/ CNAME index.html favicon.png
-git commit -m "Deploying fresh v$COMMIT"
+git commit -m "Deploying version $COMMIT to gh-pages"
 git push origin gh-pages
 
 # Switch back to master branch
