1ad01364be7606df9e20f923158cfda0d5270768
freya <fb@cix.ie>
2019-07-17 17:34:37 +0100

    Create and handle a new noJS warning message

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
 import { Fresh } from './fresh'
 
 const fresh = new Fresh()
+
+// Remove the noJS warning
+const noJSWarning = document.getElementById('no-js')!
+noJSWarning.parentElement!.removeChild(noJSWarning)
