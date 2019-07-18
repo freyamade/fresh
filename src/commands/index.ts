724382d831cd796a7578c0034fa93395a542c0ef
freyamade <frybrdrck@gmail.com>
2019-07-19 00:18:31 +0100

    Added clear command

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -4,13 +4,15 @@ import { CommandMapping, OutputFactory } from 'javascript-terminal'
 // local
 import { Cat } from './cat'
 import { CD } from './cd'
+import { Clear } from './clear'
 import { Help } from './help'
 import { Summary } from './summary'
 
 // This is not automatically ordered, need to keep track of ordering myself
 export const Commands = CommandMapping.create({
-  '?':    Summary,
-  'cat':  Cat,
-  'cd': CD,
-  'help': Help,
+  '?':     Summary,
+  'cat':   Cat,
+  'cd':    CD,
+  'clear': Clear,
+  'help':  Help,
 })
