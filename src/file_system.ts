453bb46e1a274fd41660ccc85b1dabd5282b5246
freyamade <frybrdrck@gmail.com>
2019-07-18 23:47:34 +0100

    Fixing URL in Crcophony file

--- src/file_system.ts
+++ src/file_system.ts
@@ -9,6 +9,6 @@ export const FileSystem = FS.create({
     content: `<p># <span class="yellow underline">crcophony</span></p>
     <p>A Terminal UI for Discord written in Crystal.</p>
     <br />
-    <p><span class="green">GitHub - <span class="underline">https://github.com/freyamade/crcophony</span></span>`
+    <p><span class="green">GitHub - <a href="https://github.com/freyamade/crcophony">https://github.com/freyamade/crcophony</a></span>`
   },
 })
