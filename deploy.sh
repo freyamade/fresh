a48bfda591d7954f78226c95270b821f1564e427
freyamade <frybrdrck@gmail.com>
2019-07-20 01:24:18 +0100

    Deploy.sh should use the latest master commit

--- deploy.sh
+++ deploy.sh
@@ -3,13 +3,15 @@
 # Fail out on first error
 set -e
 
+# Get the latest master commit
+COMMIT=$(git rev-parse --short HEAD)
+
 # Checkout gh-pages and pull master into it
 git checkout gh-pages
 git fetch origin
 git rebase origin master
 
 # Before running the build, take the latest commit and insert it into the code
-COMMIT=$(git rev-parse --short HEAD)
 sed -i "s/{VERSION}/$COMMIT/" src/index.ts
 
 # Build the static files on this branch first
