fd2d12b6ec666b2cd7cb1875355bc83636fbfb5c
freyamade <frybrdrck@gmail.com>
2019-07-20 01:31:21 +0100

    still trying to fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ COMMIT=$(git rev-parse --short HEAD)
 # Checkout gh-pages and pull master into it
 git checkout gh-pages
 git fetch origin
-git rebase origin master
+git pull origin master
 
 # Before running the build, take the latest commit and insert it into the code
 sed -i "s/{VERSION}/$COMMIT/" src/index.ts
@@ -21,9 +21,9 @@ NODE_ENV=production npm run build
 git checkout -- src/index.ts
 
 # Add the necessary files for the gh-pages branch and commit them
-# git add --all
-# git commit -m "Deploying version $COMMIT to gh-pages"
-# git push origin gh-pages
+git add --all
+git commit -m "Deploying version $COMMIT to gh-pages"
+git push origin gh-pages
 
-# # Switch back to master branch
-# git checkout master
+# Switch back to master branch
+git checkout master
