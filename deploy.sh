2720f4674236748300c23edf53f86d4708e71e70
freya <fb@cix.ie>
2019-07-31 15:56:54 +0100

    Trying to figure out how to deploy to a different repo

--- deploy.sh
+++ deploy.sh
@@ -9,16 +9,6 @@ set +v
 # Get the latest master commit
 VERSION=$(date +'%Y.%m.%d')
 
-# Fix git fetch
-git config --replace-all remote.origin.fetch +refs/heads/*:refs/remotes/origin/*
-
-# Checkout proper master
-git fetch origin
-git checkout master
-
-# Update the remote URL with the access token needed to push
-git remote set-url origin "https://freyamade:${GH_TOKEN}@github.com/freyamade/freyamade.github.io.git"
-
 # Builds will now be done from master in fresh to master in the pages repo
 
 # Before running the build, take the latest commit and insert it into the code
@@ -27,11 +17,18 @@ sed -i "s/{VERSION}/$VERSION/" src/index.ts
 # Build the static files on this branch first
 NODE_ENV=production npm run build
 
-# Remove the change to index.ts
-git checkout -- src/index.ts
+# -------------------------------------------------------------------------------
+# Deployment steps
 
-# Add the necessary files for the gh-pages branch and commit them
+# Clone the private repo next to this one
+git clone "https://freyamade:${GH_TOKEN}@github.com/freyamade/freyamade.github.io.git" ../public
+
+# Copy the necessary files into the public directory
+cp -r static ../public
+cp *.html ../public
+
+# Commit the changes in the public repo and push it
+cd ../public
 git add --all
-git add -f static
-git commit -m "Deploying version '$VERSION'" || true
+git commit -m "Deploying $VERSION"
 git push origin master
