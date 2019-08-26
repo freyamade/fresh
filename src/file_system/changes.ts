52e67777d9ac49d45fdea33a0a5f63a74c8d73ab
freya <fb@cix.ie>
2019-08-26 18:08:03 +0100

    Fix typo in changes.ts file

--- src/file_system/changes.ts
+++ src/file_system/changes.ts
@@ -1,5 +1,5 @@
 export const Changes = `<p># <span class="yellow underline">Latest Changes</span></p>
 <p>## <span class="cyan underline">26.08.2019</span><p>
 <p><span class="green">-</span> "pkg" command implemented..</p>
-<p><span class="green">-</span> The `pkg` command allows for installation of extra packages to add extra functionality to the website.</p>
+<p><span class="green">-</span> The "pkg" command allows for installation of extra packages to add extra functionality to the website.</p>
 <p><span class="green">-</span> There aren't any useful packages in the system yet, but the feature is there now and that's the main thing.</p>`
