a67bbf3b2747a1115e04de759df9b7ec104301f5
freya <fb@cix.ie>
2019-08-29 17:12:52 +0100

    Another small changelog update

--- src/file_system/changes.ts
+++ src/file_system/changes.ts
@@ -3,4 +3,5 @@ export const Changes = `<p># <span class="yellow underline">Latest Changes</span
 <p><span class="green">-</span> "game-of-life" pkg added.</p>
 <p><span class="green">&nbsp;&nbsp;-</span> "game-of-life" contains a single command, "life", which runs a Game of Life simulation.</p>
 <p><span class="green">&nbsp;&nbsp;-</span> This Game of Life is written in Rust and built using Web Assembly.</p>
-<p><span class="green">-</span> "test" pkg removed.</p>`
+<p><span class="green">-</span> "test" pkg removed.</p>
+<p><span class="green">-</span> Terminal rendering improved by removing repeated work.</p>`
