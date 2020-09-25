c1f66b2524344ca361080ff61db2060d59b13e96
freyamade <frybrdrck@gmail.com>
2020-09-25 16:30:54 +0100

    Added green colour to - bullets for consistency

--- src/file_system/changes.ts
+++ src/file_system/changes.ts
@@ -5,5 +5,5 @@ export const Changes = `<p># <span class="yellow underline">Latest Changes</span
 <p><span class="green">&nbsp;&nbsp;-</span> added experience dir.</p>
 <p><span class="green">&nbsp;&nbsp;-</span> removed the fact that I'm no longer working in CIX.</p>
 <p><span class="green">&nbsp;&nbsp;-</span> added more details to "whoami".</p>
-<p><span class="green">-</span> added a `cv` command;</p>
-<p><span class="green">&nbsp;&nbsp;-</span>opens my CV as a pdf in a new window.</p>`
+<p><span class="green">-</span> added a "cv" command;</p>
+<p><span class="green">&nbsp;&nbsp;-</span> opens my CV as a pdf in a new window.</p>`
