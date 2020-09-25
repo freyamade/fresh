cfdf27dd66cb75131475fb7b166b16621b1e2ef5
freyamade <frybrdrck@gmail.com>
2020-09-25 15:35:06 +0100

    File System changes
    - Added experience
    - Divided files into dirs

---
+++ src/file_system/experience/netsoc.ts
@@ -0,0 +1,7 @@
+export const Netsoc = `<p># <span class="yellow underline">UCC Netsoc</span> - <span class="underline">SysAdmin / ExOfficio (September 2017 - May 2019)</span></p>
+<p> - Introduced Docker to the SysAdmin team and helped move services to using it.</p>
+<p> - Assisted in the rewrite of the student portal from PHP into Python using Flask.</p>
+<p> - Made various user-facing improvements, including adding the ability for students to choose their preferred shell for logging into the servers.</p>
+<p> - Gave several techtalks throughout my two years of being a part of the society.</p>
+<br />
+<p><span class="green">Tech Talk Slides - <a href="https://freyamade.netsoc.co">https://freyamade.netsoc.co</a></span>`
--- src/file_system/index.ts
+++ src/file_system/index.ts
@@ -1,18 +1,23 @@
 // lib
 import { FileSystem as FS } from 'javascript-terminal'
 // local
+import { CloudCIX } from './experience/cix'
+import { Crcophony } from './projects/crcophony'
+import { Drizzle } from './projects/drizzle'
+import { Fresh } from './projects/fresh'
+import { GithubUserLanguages } from './projects/ghl'
 import { Changes } from './changes'
-import { Crcophony } from './crcophony'
-import { Drizzle } from './drizzle'
-import { Fresh } from './fresh'
-import { GithubUserLanguages } from './ghl'
 import { LearningPlans } from './learning'
 
 // Create a FileSystem for the terminal
 export const FileSystem = FS.create({
-  '/': {canModify: false},
+  '/': {},
+  '/freyama.de': {home: true},
   '/freyama.de/.changes': {content: Changes},
   '/freyama.de/learning_plans': {content: LearningPlans},
+  '/freyama.de/experience': {},
+  '/freyama.de/experience/cloudcix': {content: CloudCIX},
+  '/freyama.de/projects': {},
   '/freyama.de/projects/crcophony': {content: Crcophony},
   '/freyama.de/projects/drizzle': {content: Drizzle},
   '/freyama.de/projects/freyama.de': {content: Fresh},
