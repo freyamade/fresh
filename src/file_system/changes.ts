e3cfb0f698ce60ad8b7b01b326e3d61555eefae5
freya <fb@cix.ie>
2021-07-05 13:06:42 +0100

    silly lil ts error

--- src/file_system/changes.ts
+++ src/file_system/changes.ts
@@ -2,4 +2,4 @@ export const Changes = `<p># <span class="yellow underline">Latest Changes</span
 <p>## <span class="cyan underline">05.07.2021</span><p>
 <p><span class="green">-</span> Dependabot alert fixes.</p>
 <p><span class="green">-</span> Updated libraries to latest versions.</p>
-<p><span class="green">-</span> Updated details about current employment.</p>
+<p><span class="green">-</span> Updated details about current employment.</p>`
