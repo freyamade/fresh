3e03df698eb7513eb3c3559131248e1415aa96bf
freyamade <frybrdrck@gmail.com>
2023-08-13 14:46:31 +0100

    updating experience / project data

--- src/file_system/experience/n8n.ts
+++ src/file_system/experience/n8n.ts
@@ -1,1 +1,4 @@
-export const N8N = `<p># <span class="yellow underline">N8N.io</span> - <span class="underline">Software Engineer (August 2022 - current)</span></p>`
+export const N8N = `<p># <span class="yellow underline">N8N.io</span> - <span class="underline">Software Engineer (August 2022 - February 2023)</span></p>
+<p><span class="green">-</span> Developed a system of tracking workflow execution and http request statistics for individual workflows.</p>
+<p><span class="green">-</span> Fixed issues with multi-user workflow editing that prevented users from collaborating successfully.</p>
+<p><span class="green">-</span> Added API endpoints for checking licensing and usage information.</p>`
