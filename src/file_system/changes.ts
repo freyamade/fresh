bc07817b577313f9be0e4e3c93efb2728ee7adee
freyamade <frybrdrck@gmail.com>
2023-08-13 14:46:37 +0100

    updating changelog

--- src/file_system/changes.ts
+++ src/file_system/changes.ts
@@ -1,5 +1,6 @@
 export const Changes = `<p># <span class="yellow underline">Latest Changes</span></p>
-<p>## <span class="cyan underline">07.16.22</span><p>
+<p>## <span class="cyan underline">2023.08.13</span><p>
 <p><span class="green">-</span> Dependabot alert fixes.</p>
 <p><span class="green">-</span> Updated libraries to latest versions.</p>
-<p><span class="green">-</span> Updated lots of details since last year.</p>`
+<p><span class="green">-</span> Updated lots of details since last year.</p>
+<p><span class="green">-</span> Moved CV url to better follow my URL scheme.</p>`
