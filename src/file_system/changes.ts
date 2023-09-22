99ba887de46fa9e480f8ffa438d13f7fca2feef0
freyamade <frybrdrck@gmail.com>
2023-09-22 17:18:03 +0100

    number experience files for proper ordering

--- src/file_system/changes.ts
+++ src/file_system/changes.ts
@@ -1,6 +1,7 @@
 export const Changes = `<p># <span class="yellow underline">Latest Changes</span></p>
 <p>## <span class="cyan underline">22.09.2023</span><p>
 <p><span class="green">-</span> Dependabot alert fixes.</p>
+<p><span class="green">-</span> Files in \`experience\` now have numbers to sort them newest first.</p>
 <p><span class="green">-</span> Minor restyle, colours updated to match my newer preferences.</p>
 <p><span class="green">-</span> Added the \`sitemap\` command to display urls that host other projects.</p>
 <p><span class="green">-</span> Updated details about current employment.</p>`
