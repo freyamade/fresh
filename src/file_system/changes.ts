cafa29a440297b9ac3ac28957c9a235d79096cc1
freyamade <freya.broderick@honu.ai>
2024-11-22 15:07:06 +0100

    update file system

--- src/file_system/changes.ts
+++ src/file_system/changes.ts
@@ -1,7 +1,8 @@
 export const Changes = `<p># <span class="yellow underline">Latest Changes</span></p>
 <p>## <span class="cyan underline">2024.11.22</span><p>
-<p><span class="green">-</span> Updated files all over the system with latest updates.</p>
+<p><span class="green">-</span> Updated filesystem with updates since the previous deployment;</p>
+<p>&nbsp;&nbsp;<span class="green">-</span> Added new experience info, new project details.</p>
+<p>&nbsp;&nbsp;<span class="green">-</span> Removed some not-so-relevant-anymore files.</p>
 <p><span class="green">-</span> Improved tab completion for handling directories.</p>
-<p><span class="green">-</span> Added new  <span class="yellow">savage-aim</span> package to interact with the site from the terminal.</p>
 <p><span class="green">-</span> This will likely be the last version of <span class="magenta">freshv1</span>.</p>
 <p>&nbsp;&nbsp;<span class="green">-</span> <span class="magenta">freshv2</span> is in the pipeline for 2025, custom built and better than ever!</p>`
--- src/file_system/index.ts
+++ src/file_system/index.ts
@@ -6,16 +6,16 @@ import { CloudCIX } from './experience/cix'
 import { Honu } from './experience/honu'
 import { N8N } from './experience/n8n'
 import { Netsoc } from './experience/netsoc'
-import { Crcophony } from './projects/crcophony'
-import { Drizzle } from './projects/drizzle'
 import { Fresh } from './projects/fresh'
 import { GithubUserLanguages } from './projects/ghl'
+import { SavageAIIM } from './projects/savage-ai-im'
 import { SavageAim } from './projects/savage-aim'
 import { LearningPlans } from './learning'
 
 // Create a FileSystem for the terminal
 export const FileSystem = FS.create({
   '/': {},
+  '/tmp': {},
   '/freyama.de': {home: true},
   '/freyama.de/.changes': {content: Changes},
   '/freyama.de/learning_plans': {content: LearningPlans},
@@ -25,9 +25,8 @@ export const FileSystem = FS.create({
   '/freyama.de/experience/3-cloudcix': {content: CloudCIX},
   '/freyama.de/experience/4-netsoc': {content: Netsoc},
   '/freyama.de/projects': {},
-  '/freyama.de/projects/crcophony': {content: Crcophony},
-  '/freyama.de/projects/drizzle': {content: Drizzle},
   '/freyama.de/projects/freyama.de': {content: Fresh},
   '/freyama.de/projects/github-user-languages': {content: GithubUserLanguages},
   '/freyama.de/projects/savage-aim': {content: SavageAim},
+  '/freyama.de/projects/savage-ai-im': {content: SavageAIIM},
 })
