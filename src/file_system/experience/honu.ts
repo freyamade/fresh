5c5a97fc932685c3e651649ab2080580aafa04cd
freyamade <freya.broderick@honu.ai>
2024-11-22 14:56:34 +0100

    update honu experience file

--- src/file_system/experience/honu.ts
+++ src/file_system/experience/honu.ts
@@ -1,2 +1,4 @@
 export const Honu = `<p># <span class="yellow underline">honu.ai</span> - <span class="underline">Mid Level Software Engineer, September 2023 - Present</span></p>
-<p><span class="green">-</span> Not enough data to populate this file yet.</p>`
+<p><span class="green">-</span> One of the two core Python engineers in charge of development of a PaaS offering.</p>
+<p><span class="green">-</span> Assistant frontend developer using TypeScript and React.</p>
+<p><span class="green">-</span> One of a core team of developers building out Agents to aid users of the PaaS system.</p>`
