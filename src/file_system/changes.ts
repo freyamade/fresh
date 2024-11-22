d54cc36f89e963a589c1c9d792424673f4890bd7
freyamade <freya.broderick@honu.ai>
2024-11-22 14:21:14 +0100

    updated changelog

--- src/file_system/changes.ts
+++ src/file_system/changes.ts
@@ -1,15 +1,7 @@
 export const Changes = `<p># <span class="yellow underline">Latest Changes</span></p>
-<p>## <span class="cyan underline">2023.09.27</span><p>
-<p><span class="green">-</span> Improved <span class="yellow">pkg</span> installation.</p>
-<p>&nbsp;&nbsp;<span class="green">-</span> Now installs if the given filter finds exactly 1 package.</p>
-<p><span class="green">-</span> The <span class="yellow">?</span> command now displays installed packages separately.</p>
-<br />
-<p>## <span class="cyan underline">2023.09.26</span><p>
-<p><span class="green">-</span> Minor update to add the <span class="green">github-user-languages</span> package.</p>
-<br />
-<p>## <span class="cyan underline">2023.09.22</span><p>
-<p><span class="green">-</span> Dependabot alert fixes.</p>
-<p><span class="green">-</span> Files in \`experience\` now have numbers to sort them newest first.</p>
-<p><span class="green">-</span> Minor restyle, colours updated to match my newer preferences.</p>
-<p><span class="green">-</span> Added the \`sitemap\` command to display urls that host other projects.</p>
-<p><span class="green">-</span> Updated details about current employment.</p>`
+<p>## <span class="cyan underline">2024.11.22</span><p>
+<p><span class="green">-</span> Updated files all over the system with latest updates.</p>
+<p><span class="green">-</span> Improved tab completion for handling directories.</p>
+<p><span class="green">-</span> Added new  <span class="yellow">savage-aim</span> package to interact with the site from the terminal.</p>
+<p><span class="green">-</span> This will likely be the last version of <span class="magenta">freshv1</span>.</p>
+<p>&nbsp;&nbsp;<span class="green">-</span> <span class="magenta">freshv2</span> is in the pipeline for 2025, custom built and better than ever!</p>`
