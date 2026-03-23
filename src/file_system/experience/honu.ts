185cb7c00c14ebab017cb9ffed61e8beca441f83
freyamade <frybrdrck@gmail.com>
2026-03-23 16:15:03 +0100

    update honu experience

--- src/file_system/experience/honu.ts
+++ src/file_system/experience/honu.ts
@@ -1,4 +1,10 @@
-export const Honu = `<p># <span class="yellow underline">honu.ai</span> - <span class="underline">Mid Level Software Engineer, September 2023 - Present</span></p>
-<p><span class="green">-</span> One of the two core Python engineers in charge of development of a PaaS offering.</p>
-<p><span class="green">-</span> Assistant frontend developer using TypeScript and React.</p>
-<p><span class="green">-</span> One of a core team of developers building out Agents to aid users of the PaaS system.</p>`
+export const Honu = `<p># <span class="yellow underline">honu.ai</span> - <span class="underline">Senior Software Engineer & Backend Tech Lead, September 2023 - March 2026</span></p>
+<p><span class="green">-</span> Managed the building and rollout of the entire Honu PaaS and Honulabs SaaS systems onto GCP Cloud Run using Terraform and Terragrunt.</p>
+<p><span class="green">-</span> Helped the development of the Honu Agent platform; a contextually powered system for the cooperation of AI Agents using a centralised Business Context.</p>
+<p><span class="green">-</span> Developed a Credntial Proxying system in the platform, to allow User's services to be used by Agents they've employed without directly providing them credential information.</p>
+<p><span class="green">-</span> Developed a Data Proxy system, to allow Agents to create, update, or query Databases in Bigquery (or other backends), and make them available to other Agents for data sharing.</p>
+<p><span class="green">-</span> Developed the Honulabs API in FastAPI; A Rest API with a resilient long running task system which uses LLMs to turn a SaaS Business Idea into everything needed to get started.</p>
+<p><span class="green">-</span> Developed custom Chat infrastructure linked to the Honu Agent Platform, allowing Users to talk to their Agents in context, and to link to specific points of Context to talk about.</p>
+<p><span class="green">-</span> Developed an Agent Task infrastructure in the platform, which allows for scheduled running of regular tasks for each agent, and for agents to check what other tasks are scheduled and their statuses.</p>
+<p><span class="green">-</span> Built and developed different pipelines for ingestion of data from the User's services into the Data Proxy system, using Airflow DAGs and DBT transformations.</p>
+`
