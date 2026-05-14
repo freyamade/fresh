225cafc28a4a213cb73c22ea92fe2efceaf0e8de
freyamade <frybrdrck@gmail.com>
2026-05-14 22:42:33 +0200

    removed vitest stuff

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,7 +1,6 @@
 import { globalIgnores } from 'eslint/config'
 import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
 import pluginVue from 'eslint-plugin-vue'
-import pluginVitest from '@vitest/eslint-plugin'
 import pluginOxlint from 'eslint-plugin-oxlint'
 import skipFormatting from 'eslint-config-prettier/flat'
 
@@ -20,11 +19,6 @@ export default defineConfigWithVueTs(
 
   ...pluginVue.configs['flat/essential'],
   vueTsConfigs.recommended,
-
-  {
-    ...pluginVitest.configs.recommended,
-    files: ['src/**/__tests__/*'],
-  },
 
   ...pluginOxlint.buildFromOxlintConfigFile('.oxlintrc.json'),
 
